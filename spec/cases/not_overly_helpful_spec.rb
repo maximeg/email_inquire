f72d5af73c842ffc90b1b079e672cede41166989
Maxime Garcia <maxime.garcia@gmail.com>
2017-03-20 08:21:50 +0100

    Spec: Add a not overly helpful case

---
+++ spec/cases/not_overly_helpful_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+describe "Case: Not overly helpful" do
+  %w(
+    john.doe@domain.ca
+  ).each do |kase|
+    it "does not propose a hint for #{kase}" do
+      response = EmailInquire.validate(kase)
+      expect(response.status).to eq(:valid)
+    end
+  end
+end
