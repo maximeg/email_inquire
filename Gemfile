defa40e22be0bdef0f832d56cf83ada491660b06
Maxime Garcia <maxime.garcia@gmail.com>
2017-05-31 23:03:59 +0200

    CI: Add code coverage

--- Gemfile
+++ Gemfile
@@ -10,3 +10,7 @@ group :local do
   gem "guard-rspec", require: false
   gem "terminal-notifier-guard", require: false # OS X
 end
+
+# Code coverage
+gem "codecov", require: false
+gem "simplecov", require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start do
+  add_filter("spec/")
+end
+
+if ENV["CI"] == "true"
+  require "codecov"
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require "bundler/setup"
 require "email_inquire"
 
